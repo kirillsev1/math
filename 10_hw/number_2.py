be1c6ba2825ef1738cbd78c1c3dc7c7d42715273
kirillsev1 <kirillprih47@gmail.com>
2023-10-09 22:34:15 +0300

    added 10 hw

--- 10_hw/number_2.py
+++ 10_hw/number_2.py
@@ -1,26 +1,29 @@
 import matplotlib.pyplot as plt
 
+INTERPOLATION_FACTOR = 6
+
 
 def build_plot(x_points, y_points, s_0, s_4):
     delta_x = [x_points[i + 1] - x_points[i] for i in range(len(x_points) - 1)]
     delta_y = [y_points[i + 1] - y_points[i] for i in range(len(y_points) - 1)]
 
     second_derivatives = [0] * len(x_points)
-    second_derivatives[0] = 6 / delta_x[0] * (delta_y[0] / delta_x[0] - s_0)
+    second_derivatives[0] = INTERPOLATION_FACTOR / delta_x[0] * (delta_y[0] / delta_x[0] - s_0)
 
     for i in range(1, len(x_points) - 1):
-        second_derivatives[i] = (6 / (delta_x[i - 1] + delta_x[i]) *
-                                 (delta_y[i] / delta_x[i] - delta_y[i - 1] / delta_x[i - 1]))
+        divided_y = delta_y[i] / delta_x[i] - delta_y[i - 1] / delta_x[i - 1]
+        second_derivatives[i] = INTERPOLATION_FACTOR / (delta_x[i - 1] + delta_x[i]) * divided_y
 
-    second_derivatives[-1] = 6 / delta_x[-1] * (s_4 - delta_y[-1] / delta_x[-1])
+    second_derivatives[-1] = INTERPOLATION_FACTOR / delta_x[-1] * (s_4 - delta_y[-1] / delta_x[-1])
 
     a = y_points[:-1]
     b = [0] * len(x_points)
     d = [0] * len(x_points)
 
     for i in range(len(x_points) - 1):
-        b[i] = delta_y[i] / delta_x[i] - delta_x[i] * (2 * second_derivatives[i] + second_derivatives[i + 1]) / 6
-        d[i] = (second_derivatives[i + 1] - second_derivatives[i]) / (6 * delta_x[i])
+        coefficient = 2 * second_derivatives[i] + second_derivatives[i + 1]
+        b[i] = delta_y[i] / delta_x[i] - delta_x[i] * coefficient / INTERPOLATION_FACTOR
+        d[i] = (second_derivatives[i + 1] - second_derivatives[i]) / (INTERPOLATION_FACTOR * delta_x[i])
 
     x_spline = [x / 100 for x in range(400)]
     y_spline = [0] * len(x_spline)
@@ -29,8 +32,8 @@ def build_plot(x_points, y_points, s_0, s_4):
         for j in range(len(x_spline)):
             if x_points[i] <= x_spline[j] <= x_points[i + 1]:
                 x_interval = x_spline[j] - x_points[i]
-                y_spline[j] = (a[i] + b[i] * x_interval +
-                               second_derivatives[i] / 2 * x_interval ** 2 + d[i] * x_interval ** 3)
+
+                y_spline[j] = a[i] + b[i] * x_interval + second_derivatives[i] / 2 * x_interval ** 2 + d[i] * x_interval ** 3
 
     plt.plot(x_points, y_points, "o", label="Исходные точки")
     plt.plot(x_spline, y_spline, label="Кубический сплайн")
